9c30eeab47feac7455c0334df1e707694fbc4f9f
Leah Kelley <121841278+LeahKelley@users.noreply.github.com>
2026-03-22 12:58:06 -0400

    Sheng dev (#8)
    * installed dependencies
    
    * revised
    
    * revised again
    
    * added backend + 2 endpoints
    
    * Enhance API spec with field rules and response details (#1)
    
    Added request and response field rules for API v1, including required fields and their formats. Updated response structure to include additional details for celestial bodies.
    
    * Leah dev (#2)
    
    * Enhance API spec with field rules and response details
    
    Added request and response field rules for API v1, including required fields and their formats. Updated response structure to include additional details for celestial bodies.
    
    * Revise API spec for Natal Chart and Health endpoints
    
    Updated the API specification to improve clarity and organization of request and response fields.
    
    * Add code owners (#4)
    
    * Update .gitignore to exclude venv and ephemeris data (#3)
    
    * Update .gitignore to exclude venv and ephemeris data
    and models.py creation.
    
    * Fix api-spec.md file in /docs
    
    * readded requirements.txt after reinstalling venv
    
    * LEAH task1 (#6)
    
    * TASK 1 - SHENG (Scaffolding set up for FASTAPI)
    
    * add a test frontend page to make sure backend is working - LK
    
    * create testing page
    
    * building test front end
    
    * created html frintend to view
    
    * updated multiple files from test frontend to match backend as is
    
    * chnages to connect backend to frontend
    
    * Implemented all backend tasks previously assigned to Sheng
    
    * started database setup
    
    * location to long/lat implemented
    
    * added long/lat --> timezone
    
    * fixed typo
    
    * Delete apps/client-ui/src/pages/TestNatalPage.tsx
    
    FIXING PULL REQUEST FOR MERGE
    
    * Delete apps/client-ui/src/App.tsx
    
    FIXING PULL REQUEST FOR MERGE
    
    * Delete apps/client-ui/src/main.tsx
    
    FIXING PULL REQUEST FOR MERGE
    
    * Delete apps/client-ui/index.html
    
    FIXING PULL REQUEST FOR MERGE
    
    ---------
    
    Co-authored-by: Shenghao <shenghaodong18@gmail.com>

---
+++ services/geolocation/geolocation.py
@@ -0,0 +1,40 @@
+import os
+import swisseph as swe
+from datetime import datetime
+from fastapi import FastAPI, Query, HTTPException
+from geopy.geocoders import Nominatim
+from geopy.extra.rate_limiter import RateLimiter
+from zoneinfo import ZoneInfo
+from timezonefinder import TimezoneFinder
+
+app = FastAPI(
+    title="Geolocation",
+    description="Geolocation services for retrieving location data",
+)
+
+geolocator = Nominatim(user_agent="Location Data") 
+tf = TimezoneFinder()
+geocode = RateLimiter(geolocator.geocode, min_delay_seconds=1)
+
+@app.get("/location/geolocation")
+def location(address: str):
+    locationData = []
+    getLocation = geolocator.geocode(address)
+    if getLocation is None:
+        raise HTTPException(
+            status_code=404, 
+            detail=f"Address '{address}' could not be found. "
+        )
+    locationData.append({"latitude": getLocation.latitude, "longitude": getLocation.longitude})
+    return {'Latitude and Longitude': locationData}
+
+@app.get("/location/geolocation/timezone")
+def timezone(lat: float, lng: float):
+    timezoneName = tf.timezone_at(lng=lng, lat=lat)
+
+    timeNow = datetime.now(ZoneInfo(timezoneName))
+    convertedTime = timeNow.utcoffset().total_seconds() / 3600 #Convert time to seconds then hour to find UTC Time
+    return {
+        "timezone": timezoneName, #Timezone Name
+        "utc_offset": convertedTime #UTC Time
+    }
